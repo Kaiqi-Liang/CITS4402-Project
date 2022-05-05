51ed9ea004f281248fc7a259a440bc1d1eedcb7a
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-05-05 10:33:05 +0800

    Remove parser

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from match import candidate_match_discrimination
 
 parser = Parser('mot/car/001', '{:06}.jpg', (1, 4))
 objects = candidate_small_objects_detection(parser)
-candidate_match_discrimination(parser, objects)
+candidate_match_discrimination(objects)
--- match.py
+++ match.py
@@ -3,7 +3,7 @@ import cv2
 import scipy as sp 
 import numpy as np
 import skimage
-def candidate_match_discrimination(parser: Parser, candidate_small_objects: list[np.ndarray]):
+def candidate_match_discrimination(candidate_small_objects: list[np.ndarray]):
 	for object in candidate_small_objects:
 		labelled_image = skimage.measure.label(object)
 		properties = skimage.measure.regionprops(labelled_image)
