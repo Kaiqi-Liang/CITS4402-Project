094a29023967fb3bde51a9bed8ae2d644cdd492c
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-05-06 13:18:45 +0800

    Split the image into 30x30 pixels

--- detection.py
+++ detection.py
@@ -1,6 +1,7 @@
 # Candidate small object detection: Motion-Based Detection Using Local Noise Modelling Algorithm 
 
 #import relevant packages 
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
@@ -19,26 +20,41 @@ def candidate_small_objects_detection(parser: Parser) -> list[np.ndarray]:
 		gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 		gray3 = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
 
-		#(1) Inter-frame differences
-		diff_img_12 = cv2.absdiff(gray2, gray1)
-		diff_img_23 = cv2.absdiff(gray3, gray2)
+		binary_rows = []
+		for i in range(len(gray1) // 30 + 1):
+			binary_cols = []
+			for j in range(len(gray1) // 30 + 1):
+				gray1_split = gray1[30 * i:30 * (i + 1), 30 * j:30 * (j + 1)]
+				gray2_split = gray2[30 * i:30 * (i + 1), 30 * j:30 * (j + 1)]
+				gray3_split = gray3[30 * i:30 * (i + 1), 30 * j:30 * (j + 1)]
 
-		#(2) Thresholding: convert greyscale to binary 
-		pfa = 0.05
-		th_12 = - np.log(pfa) * np.mean(diff_img_12)
-		th_23 = - np.log(pfa) * np.mean(diff_img_23)
-		thresh_img_12 = cv2.threshold(diff_img_12, th_12, 255, cv2.THRESH_BINARY)[1]
-		thresh_img_23 = cv2.threshold(diff_img_23, th_23, 255, cv2.THRESH_BINARY)[1]
+				#(1) Inter-frame differences
+				diff_img_12 = cv2.absdiff(gray2_split, gray1_split)
+				diff_img_23 = cv2.absdiff(gray3_split, gray2_split)
 
-		# plt.imshow(thresh_img_12, cmap='gray')
-		# plt.title("Thresholded Image: 12")
-		# plt.show()
+				#(2) Thresholding: convert greyscale to binary
+				pfa = 0.05
+				th_12 = - np.log(pfa) * np.mean(diff_img_12)
+				th_23 = - np.log(pfa) * np.mean(diff_img_23)
+				thresh_img_12 = cv2.threshold(diff_img_12, th_12, 255, cv2.THRESH_BINARY)[1]
+				thresh_img_23 = cv2.threshold(diff_img_23, th_23, 255, cv2.THRESH_BINARY)[1]
 
-		#(3) Candidate Extractions: Logical AND 
-		thresh_and = np.logical_and(thresh_img_12, thresh_img_23) * 255
-		res.append(thresh_and)
+				# plt.imshow(thresh_img_12, cmap='gray')
+				# plt.title("Thresholded Image: 12")
+				# plt.show()
 
-		# plt.imshow(thresh_and, cmap='gray')
+				#(3) Candidate Extractions: Logical AND
+				thresh_and = np.logical_and(thresh_img_12, thresh_img_23) * 255
+				binary_cols.append(thresh_and)
+			binary_rows.append(binary_cols)
+		binary_images = []
+
+		# merge the 30x30 split images back into 1 binary image
+		for row in binary_rows:
+			binary_images.append(np.array([list(itertools.chain(*col)) for col in zip(*row)]))
+		res.append(np.concatenate(binary_images))
+
+		# plt.imshow(res[0], cmap='gray')
 		# plt.title("logical AND")
 		# plt.show()
 
