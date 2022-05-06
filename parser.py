27d3588cfc9da6f80237f13837bd86a3fcbfa0d5
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-05-06 08:01:23 +0800

    Improve parser

--- parser.py
+++ parser.py
@@ -1,9 +1,16 @@
 import numpy as np
+import os
 import cv2
 class Parser:
-	def __init__(self, folder: str, template: str, frame_range: tuple[int, int]) -> None:
-		self.start, self.end = frame_range
-		self.frames = [f'{folder}/img/{template.format(frame)}' for frame in range(self.start, self.end)]
+	def __init__(self, folder: str, template: str, frame_range: tuple[int, int] = None) -> None:
+		self.folder = f'{folder}/img/'
+		if frame_range:
+			self.start, self.end = frame_range
+			self.frames = [f'{self.folder}{template.format(frame)}' for frame in range(self.start, self.end)]
+		else:
+			self.frames = sorted(os.listdir(self.folder))
+			self.start = int(self.frames[0][:self.frames[0].find('.')])
+			self.end = int(self.frames[-1][:self.frames[-1].find('.')])
 
 	def get_frame_range(self) -> tuple[int, int]:
 		return self.start, self.end
